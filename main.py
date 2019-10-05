5bbf1bdd0ff239fd6608d7ea0fb1739ee3a9cab3
chanybou0311 <chanyou0311@gmail.com>
2019-10-05 14:36:21 +0900

    [add] バッチ処理受け取る用のエンドポイント追加

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 
+@app.route('/fetch/',  methods=['POST'])
+def fetch():
+    if request.headers['Content-Type'] != 'application/json':
+        print(request.headers['Content-Type'])
+        d = {"error": {"message": "Error: Content-Type must be application/json."}}
+        return jsonify(d), 400
+
+    data = request.json
+    if data.get("screen_name") is None:
+        d = {"error": {"message": "Error: Invalid parameter."}}
+        return jsonify(d), 400
+
+    screen_name = data.get("screen_name")
+    d = {"screen_name": screen_name}
+    return jsonify(d)
+
 @app.route('/')
-def hello_world():
-    return "Hello World!"
+def health_check():
+    d = {"status": "ok"}
+    return jsonify(d)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
